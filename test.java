0fe8e3d6ec64dc85ee0e214d79b1bec722423949
2891127887 <74778552+2891127887@users.noreply.github.com>
2020-11-23 15:17:09 +0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class test {
+	public static void main(String[] args) {
+		int i=0;
+		String[] x=new String[26];
+		Random random=new Random();
+		Scanner s=new Scanner(System.in);
+		while (i<26) {
+			x[i]=random.nextInt(100)+","+random.nextInt(100);
+			i++;
+		}
+		System.out.println("输入小写字母出随机坐标，输入其他东西错误");
+		while (true) {
+			String a=s.next();
+			char b=a.charAt(0);
+			if(Integer.valueOf(b)<123||Integer.valueOf(b)>97){
+				System.out.println(x[Integer.valueOf(b)-97]);
+				continue;
+			}
+			break;
+		}
+	}
+}
